69bbe61420b4fc68391721de38b3d3f4d155cd90
IfunanyaWilliam <williamifunanya@gmail.com>
2022-08-11 13:08:45 +0100

    Add seed data for Hotels and Countries

---
+++ HotelListing/Data/AppDbContext.cs
@@ -0,0 +1,81 @@
+﻿using HotelListing.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Country>().HasData(
+                    new Country
+                    {
+                        Id = 1,
+                        Name = "Nigeria",
+                        ShortName = "NGN"
+                    },
+                    new Country
+                    {
+                        Id = 2,
+                        Name = "Jamaica",
+                        ShortName = "JM"
+                    },
+                    new Country
+                    {
+                        Id = 3,
+                        Name = "Congo",
+                        ShortName = "CGO"
+                    },
+                    new Country
+                    {
+                        Id = 4,
+                        Name = "Cayman Island",
+                        ShortName = "CI"
+                    }
+                );
+
+            builder.Entity<Hotel>().HasData(
+                    new Hotel
+                    {
+                        Id = 1,
+                        Name = "Sheraton",
+                        Address = "Shagari Avenue, Abuja",
+                        CountryId = 1,
+                        Rating = 5
+                    },
+                    new Hotel
+                    {
+                        Id = 2,
+                        Name = "Sandal Resort and Spa",
+                        Address = "Negril, Utah",
+                        CountryId = 2,
+                        Rating = 4.5
+                    },
+                    new Hotel
+                    {
+                        Id = 3,
+                        Name = "Grand Royal Suit",
+                        Address = "Hillview, Congo",
+                        CountryId = 3,
+                        Rating = 4
+                    },
+                   new Hotel
+                   {
+                       Id = 4,
+                       Name = "Nicon Nuga",
+                       Address = "Palm Beach road, Cayman County",
+                       CountryId = 4,
+                       Rating = 5
+                   }
+                );
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
---
+++ HotelListing/Migrations/20220811120434_SeedingData.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelListing.Migrations
+{
+    public partial class SeedingData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[,]
+                {
+                    { 1, "Nigeria", "NGN" },
+                    { 2, "Jamaica", "JM" },
+                    { 3, "Congo", "CGO" },
+                    { 4, "Cayman Island", "CI" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[,]
+                {
+                    { 1, "Shagari Avenue, Abuja", 1, "Sheraton", 5.0 },
+                    { 2, "Negril, Utah", 2, "Sandal Resort and Spa", 4.5 },
+                    { 3, "Hillview, Congo", 3, "Grand Royal Suit", 4.0 },
+                    { 4, "Palm Beach road, Cayman County", 4, "Nicon Nuga", 5.0 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
