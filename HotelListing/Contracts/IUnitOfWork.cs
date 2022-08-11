4fee8b82d953c091b16e3f04112b50a542c5e737
IfunanyaWilliam <williamifunanya@gmail.com>
2022-08-11 16:42:12 +0100

    Add IUnitOfWork and implement it in UnitOfWok

---
+++ HotelListing/Contracts/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using HotelListing.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace HotelListing.Contracts
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<Country> Countries { get; }
+        IGenericRepository<Hotel> Hotels { get; }
+        Task Save();
+    }
+}
