00c6238b5bf369233748e4b37695f5e345481584
IfunanyaWilliam <williamifunanya@gmail.com>
2022-08-11 16:16:07 +0100

    Add IGenericRepository and implement it in GenericRepository

---
+++ HotelListing/Contracts/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace HotelListing.Contracts
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IList<T>> GetAll(Expression<Func<T, bool>> expression = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<string> includes = null);
+        public Task<T> Get(Expression<Func<T, bool>> expression, List<string> includes = null);
+        Task Insert(T entity);
+        Task InsertRange(IEnumerable<T> entities);
+        Task Delete(int id);
+        void DeleteRange(IEnumerable<T> entities);
+        void Update(T entity);
+    }
+}
